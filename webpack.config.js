afb66f312d8094aedfd6ca9b7b16ce9ce0def811
Yagnesh Modh <hey@yagneshmodh.com>
2022-05-09 17:35:30 +0530

    day 1 project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+  ],
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "assets"),
+      publicPath: "/assets",
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+};
